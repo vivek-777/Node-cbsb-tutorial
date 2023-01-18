488a9caf81ade18bf81e382488ba977c2f92a98d
vivek <vivekvikram777@gmail.com>
2023-01-18 17:26:24 +0530

    asynchronus handling

--- index.js
+++ index.js
@@ -93,3 +93,21 @@
 
 
 
+// --------------> branch/asynchronus
+
+
+
+// let a = 20; 
+// let b = 0;
+
+// let waitingData = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//         resolve(30);
+//     },2000);
+// });
+
+// waitingData.then((data)=>{
+//     b = data;
+//     console.log(a+b);
+// });
+
